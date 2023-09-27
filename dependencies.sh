4fb924c75f5ffc56cd428294751608749bfa9be5
Fahad Hamza <fahad.hamza@abacus-global.com>
2023-09-27 17:03:56 +0500

    dependencies.sh v0.3

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,15 @@
 sudo yum -y update &&
 sudo yum -y install epel-release &&
-sudo yum -y groupinstall "Development Tools" &&
-sudo yum -y install openssl-devel bzip2-devel libffi-devel xz-devel &&
-sudo yum -y install wget &&
-wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz &&
-tar xvf Python-3.8.12.tgz &&
-cd Python-3.8.12/ &&
-./configure --enable-optimizations &&
-sudo make altinstall &&
+# sudo yum -y groupinstall "Development Tools" &&
+# sudo yum -y install openssl-devel bzip2-devel libffi-devel xz-devel &&
+# sudo yum -y install wget &&
+# wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz &&
+# tar xvf Python-3.8.12.tgz &&
+# cd Python-3.8.12/ &&
+# ./configure --enable-optimizations &&
+# sudo make altinstall &&
+# alias python="/usr/local/bin/Python3.8" &&
+# make altinstall &&
 # sudo rm -rf altinstall &&
 # sudo yum -y install python3-pip &&
 sudo yum -y install vim dnf&&
