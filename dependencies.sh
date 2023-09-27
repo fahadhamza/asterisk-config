2f9f8ec026939e67a8d21e71b7e5232c96c40c28
Fahad Hamza <fahad.hamza@abacus-global.com>
2023-09-27 13:13:36 +0500

    dependencies.sh v0.2

--- dependencies.sh
+++ dependencies.sh
@@ -1,7 +1,16 @@
 sudo yum -y update &&
 sudo yum -y install epel-release &&
-sudo yum -y install python3-pip &&
-sudo yum -y install vim wget dnf&&
+sudo yum -y groupinstall "Development Tools" &&
+sudo yum -y install openssl-devel bzip2-devel libffi-devel xz-devel &&
+sudo yum -y install wget &&
+wget https://www.python.org/ftp/python/3.8.12/Python-3.8.12.tgz &&
+tar xvf Python-3.8.12.tgz &&
+cd Python-3.8.12/ &&
+./configure --enable-optimizations &&
+sudo make altinstall &&
+# sudo rm -rf altinstall &&
+# sudo yum -y install python3-pip &&
+sudo yum -y install vim dnf&&
 sudo pip3 install alembic ansible &&
 sudo pip3 install --upgrade pip &&
 sudo mkdir /etc/ansible &&
