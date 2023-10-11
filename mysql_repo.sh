77ccbcec904c2ff12614349ada86cc5c477560c8
Fahad Hamza <fahad.hamza@abacus-global.com>
2023-10-11 20:47:53 +0500

    mysql_repo.sh 0.1

--- mysql_repo.sh
+++ mysql_repo.sh
@@ -1,4 +1,4 @@
-sudo wget https://dev.mysql.com/downloads/repo/yum/mysql80-community-release-el8-7.noarch.rpm
-sudo rpm -ivh mysql80-community-release-el8-7.noarch.rpm
-sudo yum makecache
-sudo yum install mysql-community-server
+sudo wget https://repo.mysql.com/yum/mysql80-community-release-el9-7.noarch.rpm
+sudo rpm -ivh mysql80-community-release-el9-7.noarch.rpm
+sudo dnf makecache
+sudo dnf install mysql-community-server
