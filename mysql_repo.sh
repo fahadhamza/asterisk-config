ef3f22b478558185c1346f8af743ead1a6570c89
Fahad Hamza <fahad.hamza@abacus-global.com>
2023-10-12 12:15:32 +0500

    mysql repo update

--- mysql_repo.sh
+++ mysql_repo.sh
@@ -1,5 +1,8 @@
 # sudo wget https://repo.mysql.com/yum/mysql80-community/el/9/
-sudo wget https://dev.mysql.com/get/mysql8/repo/el9/mysql80-community-el9.repo
-sudo mv mysql80-community-el9.repo /etc/yum.repos.d/
+# sudo wget https://dev.mysql.com/get/mysql8/repo/el9/mysql80-community-el9.repo
+sudo wget https://dev.mysql.com/downloads/repo/yum/mysql80-community-release-el9-4.noarch.rpm
+sudo yum localinstall pmysql80-community-release-el9-4.noarch.rpm
+# sudo mv mysql80-community-el9.repo /etc/yum.repos.d/
 sudo dnf update
+yum repolist all | grep mysql
 # sudo dnf install mysql-community-server
