9ecdb01c48b5a7e4a8c782d8d5de1766c044fd04
Fahad Hamza <fahad.hamza@abacus-global.com>
2023-10-12 14:17:16 +0500

    mysql repo update

--- mysql_repo.sh
+++ mysql_repo.sh
@@ -1,8 +1,13 @@
-# sudo wget https://repo.mysql.com/yum/mysql80-community/el/9/
-# sudo wget https://dev.mysql.com/get/mysql8/repo/el9/mysql80-community-el9.repo
-sudo wget https://repo.mysql.com//mysql80-community-release-el9-4.noarch.rpm
-sudo dnf localinstall mysql80-community-release-el9-4.noarch.rpm
-# sudo mv mysql80-community-el9.repo /etc/yum.repos.d/
+# # sudo wget https://repo.mysql.com/yum/mysql80-community/el/9/
+# # sudo wget https://dev.mysql.com/get/mysql8/repo/el9/mysql80-community-el9.repo
+# sudo wget https://repo.mysql.com//mysql80-community-release-el9-4.noarch.rpm
+# sudo dnf localinstall mysql80-community-release-el9-4.noarch.rpm
+# # sudo mv mysql80-community-el9.repo /etc/yum.repos.d/
+# sudo dnf update
+# yum repolist all | grep mysql
+# # sudo dnf install mysql-community-server
+
+sudo wget https://dev.mysql.com/get/mysql8/repo/el9/mysql80-community-el9.repo
+sudo mv mysql80-community-el9.repo /etc/yum.repos.d/
 sudo dnf update
-yum repolist all | grep mysql
-# sudo dnf install mysql-community-server
+sudo dnf install mysql-community-server
